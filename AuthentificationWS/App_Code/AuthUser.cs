b3088f23a91bfce8b77f08496fa104f4abccf327
ReaYouFather <GRea615@gmail.com>
2019-05-21 17:21:59 -0500

    Creacion de Modelo

---
+++ AuthentificationWS/App_Code/AuthUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class AuthUser : System.Web.Services.Protocols.SoapHeader
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+
+}
