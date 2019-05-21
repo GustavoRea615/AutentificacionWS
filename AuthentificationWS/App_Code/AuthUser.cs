aaeb8db07e4081ff0d5895182b83b919d87972d4
ReaYouFather <GRea615@gmail.com>
2019-05-21 17:28:46 -0500

    Cambio 2

--- AuthentificationWS/App_Code/AuthUser.cs
+++ AuthentificationWS/App_Code/AuthUser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
 
 
 public class AuthUser : System.Web.Services.Protocols.SoapHeader
@@ -9,4 +10,11 @@ public class AuthUser : System.Web.Services.Protocols.SoapHeader
     public string UserName { get; set; }
     public string Password { get; set; }
 
+
+    public bool IsValid()
+    {
+        int count = 0;
+
+        string config = ConfigurationManager.ConnectionStrings[""].ConnectionString;
+    }
 }
